a4da02343b97d7d73253f85a9878ba6857eb7eda
TLRKiliann <philogenie@protonmail.com>
2020-07-14 17:52:23 +0200

    config

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/koala/Documents/python/pypro/serv_django/django_serv_project/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
