f64d13663f6eb9f6ec831a5e09cabc539c94267b
TLRKiliann <philogenie@protonmail.com>
2020-07-18 00:35:38 +0200

    configuration templates

--- lexical/blog/urls.py
+++ lexical/blog/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 # 127.0.0.1:8000/accueil
 urlpatterns = [
-    path('accueil/', views.home),
-    path('neuro/', views.neuro),
-    path('psycho/', views.psycho),
-    path('date/', views.date_actuelle),
-    path('addition/<int:nombre1>/<int:nombre2>/', views.addition)
+    path('base/', views.home), # pour une nouvelle app
+    path('blog/neuro/', views.neuro), # pour une nouvelle app
+    path('blog/psycho/', views.psycho), # pour une nouvelle app
+    path('blog/date/', views.date_actuelle),
+    path('blog/addition/<int:nombre1>/<int:nombre2>/', views.addition)
 ]
--- lexical/blog/views.py
+++ lexical/blog/views.py
@@ -1,35 +1,25 @@
 from datetime import datetime
-from django.shortcuts import HttpResponse
+#from django.shortcuts import HttpResponse
 from django.shortcuts import render
+
 
 def home(request):
     """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
-    return HttpResponse("""
-        <h1>Bienvenue sur mon blog !</h1>
-        <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>
-    """)
+    return render(request, 'base.html')
 
 def neuro(request):
     """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
-    return HttpResponse("""
-        <h1>Neurologie</h1>
-        <p>Symptômes</p>
-    """)
+    return render(request, 'blog/neuro.html')
 
 def psycho(request):
     """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
-    return HttpResponse("""
-        <h1>Psychiatrie</h1>
-        <p>Symptômes</p>
-    """)
+    return render(request, 'blog/psycho.html')
 
 def date_actuelle(request):
     return render(request, 'blog/date.html', {'date': datetime.now()})
-
 
 def addition(request, nombre1, nombre2):    
     total = nombre1 + nombre2
 
     # Retourne nombre1, nombre2 et la somme des deux au tpl
     return render(request, 'blog/addition.html', locals())
-    
