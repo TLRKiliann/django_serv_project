dd75d331abd33ee700dc4aa7a74ce9e1de3d1f37
TLRKiliann <philogenie@protonmail.com>
2020-07-18 13:35:44 +0200

    config models

--- lexical/blog/urls.py
+++ lexical/blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
-# 127.0.0.1:8000/accueil
+# 127.0.0.1:8000/base
 urlpatterns = [
-    path('base/', views.home), # pour une nouvelle app
-    path('blog/neuro/', views.neuro), # pour une nouvelle app
-    path('blog/psycho/', views.psycho), # pour une nouvelle app
-    path('blog/date/', views.date_actuelle),
-    path('blog/addition/<int:nombre1>/<int:nombre2>/', views.addition)
+    path('base/', views.home), # lexical/templates/base.html
+    path('blog/neuro/', views.neuro), # lexical/blog/templates/blog/neuro.html
+    path('blog/psycho/', views.psycho), # ""
+    path('blog/date/', views.date_actuelle), # ""
+    path('blog/detail/', views.product_detail_view), # ""
+    path('blog/addition/<int:nombre1>/<int:nombre2>/', views.addition) # ""
 ]
--- lexical/blog/views.py
+++ lexical/blog/views.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 #from django.shortcuts import HttpResponse
 from django.shortcuts import render
-
+from .models import Product
 
 def home(request):
     """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
@@ -23,3 +23,15 @@ def addition(request, nombre1, nombre2):
 
     # Retourne nombre1, nombre2 et la somme des deux au tpl
     return render(request, 'blog/addition.html', locals())
+
+# view of models.py
+def product_detail_view(request):
+    obj = Product.objects.get(id=1)
+    context = {
+    'title' : obj.title,
+    'description' : obj.description,
+    'price' : obj.price,
+    'summary' : obj.summary,
+    'featured' : obj.featured
+    }
+    return render(request, 'blog/detail.html', context)
